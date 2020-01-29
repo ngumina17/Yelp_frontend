d3b059b68de4bd6bb96dbe7b066556bccdb9de94
Danny Gleason <danny.c.gleason@gmail.com>
2020-01-28 21:59:26 -0500

    added styling and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from "./components/SearchBar"
+import "./App.css"
 
-function App() {
+
+class App extends React.Component{
+  constructor(){
+    super()
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log(this.state)
+
+    if (this.state !== null)
+    {this.search(this.state.location)}
+  }
+
+  search=(query)=>{
+    const url = `https://opentable.herokuapp.com/api/restaurants?city=${query}`;
+    fetch(url)
+      .then(results => results.json())
+      .then(res => {console.log(res)
+          this.setState({data: res});
+      })
+  }
+
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <header className="main-header">
+       <div className="main-header__wrapper">
+         <div className="nav-wrapper">
+            <nav className="main-nav">
+            <ul className="main-list">
+                <li class="main-list__item"><a href="#" className="main-list__link">Write a Review</a></li>
+                <li class="main-list__item"><a href="#" className="main-list__link">Events</a></li>
+                <li class="main-list__item"><a href="#" className="main-list__link">Talk</a></li>
+           </ul>
+        </nav>
+        <div className="search-container">
+          <div className="search">
+            <SearchBar onChange={this.handleChange}/>
+          </div>
+        </div>
+         </div>
+       </div>
+     </header>
+     <footer class="main-footer">
+      <small class="copyright">
+        <div class="copyright__img-block">
+          <img src="https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_styleguide/573fa19c8435/assets/img/structural/footer_cityscape.png" alt="footer img" />
+        </div>
+        <div class="copyright__text">
+          Copyright © 2004-2017 Yelp Inc. 
+          Yelp and related marks are 
+          registered trademarks of Yelp.
+        </div>
+      </small>
+    </footer>
+    </>
+      
+  )
 }
 
+}
 export default App;
--- src/components/Header_Components/SearchLocation.js
+++ src/components/Header_Components/SearchLocation.js
@@ -5,7 +5,6 @@ import React from 'react'
 function SearchLocation(props){
 
 return(
-    <input type="text" onChange={props.onChange} placeholder="Search Location..." name="location" />
 )
 }
  
