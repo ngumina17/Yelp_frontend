6681bc4394cb50fa85eaf62fef00c26cd5f1d631
KASSIDY_SHADE <kassie2129@gmail.com>
2020-01-30 14:02:47 -0600

    took out some useless code

--- src/components/Review_Form/Star_Reviews.js
+++ src/components/Review_Form/Star_Reviews.js
@@ -20,7 +20,7 @@ return(
             <input type='radio' name='star' id='star2' /><label className={`${2 <= props.rating ? 'after' : 'before'}`} onClick={stars} id='2' htmlFor='star2'></label>
             <input type='radio' name='star' id='star3' /><label className={`${3 <= props.rating ? 'after' : 'before'}`} onClick={stars} id='3' htmlFor='star3'></label>
             <input type='radio' name='star' id='star4' /><label className={`${4 <= props.rating ? 'after' : 'before'}`} onClick={stars} id='4' htmlFor='star4'></label>
-            <input type='radio' name='star' id='star5' /><label className={`${parseInt(5) <= props.rating ? 'after' : 'before'}`} onClick={stars} id='5' htmlFor='star5'></label>
+            <input type='radio' name='star' id='star5' /><label className={`${5 <= props.rating ? 'after' : 'before'}`} onClick={stars} id='5' htmlFor='star5'></label>
 </div>
 </div>
 )}
