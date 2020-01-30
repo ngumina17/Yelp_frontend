1b9d093cd1e8b5eceeb076a6f2d4534cb023940b
ngumina17 <nicole.gumina17@gmail.com>
2020-01-30 14:28:59 -0600

    working submit review

--- src/components/Review_form.js
+++ src/components/Review_form.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 import Star_Review from './Review_Form/Star_Reviews'
 import Review_Field from './Review_Form/Review_Field'
+const url = 'https://project3-restaurant-finder.herokuapp.com/restaurants/5e2c7dee634ff9000422f6da'
 
 
 function Review_Form () {
 
     const [rating, setRating] = useState({
-        rating: 0
+        rating:0 
     })
     const [text, setText] = useState({
         text: ''
@@ -19,7 +20,33 @@ function Review_Form () {
     function newText (text) {
         setText({ text: text })
     }
-console.log(rating)
+
+    function submitReview () {
+        console.log(rating)
+        fetch(url, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              mode: "no-cors"
+            },
+            body: JSON.stringify({
+              text: text.text,
+              rating: rating.rating,
+            })
+          })
+          .then(res => res.json())
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+
+    }
+    function handleClick () {
+        console.log(rating)
+        submitReview()
+    }
+
+
+
+
 return(
     <nav>
         <Star_Review  
@@ -29,9 +56,10 @@ return(
             newText={newText}
 />
 
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={handleClick}>Submit</button>
 
     </nav>
 )}
+
 
 export default Review_Form
