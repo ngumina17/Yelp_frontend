48b803b591d1d44c1b3803016cd02804e45ec714
KASSIDY_SHADE <kassie2129@gmail.com>
2020-01-31 12:10:36 -0600

    added useEffect in restraunt and added joes code to add review

--- src/components/Review_form.js
+++ src/components/Review_form.js
@@ -3,66 +3,59 @@ import Star_Review from './Review_Form/Star_Reviews'
 import Review_Field from './Review_Form/Review_Field'
 import { Button } from 'reactstrap';
 import './Form.css';
-const url = 'https://project3-restaurant-finder.herokuapp.com/restaurants/5e2c7dee634ff9000422f6da'
-
-function Review_Form () {
-
+import e from 'cors';
+const url = 'https://project3-restaurant-finder.herokuapp.com/restaurants'
+function Review_Form(props) {
     const [rating, setRating] = useState({
-        rating: 1
+        rating: 0
     })
     const [text, setText] = useState({
         text: ''
     })
-
-    function newRating (rating) {
+    function newRating(rating) {
         setRating({ rating: rating })
     }
-    function newText (text) {
+    function newText(text) {
         setText({ text: text })
     }
-
-    function submitReview () {
+    function submitReview() {
         console.log(rating)
         fetch(url, {
             method: "PUT",
             headers: {
-              "Content-Type": "application/json",
-              mode: "no-cors"
+                "Content-Type": "application/json",
+                mode: "no-cors"
             },
             body: JSON.stringify({
-              text: text.text,
-              rating: rating.rating,
+                text: text.text,
+                rating: rating.rating,
             })
-          })
-          .then(res => res.json())
-          .then(res => console.log(res))
-          .catch(err => console.log(err));
-
+        })
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+        props.history.push(`'restaurant/${props.match.param.id}`)
+        function incrementRating() {
+            rating(prevState => prevState + 1)
+        }
     }
-    function handleClick () {
+    function handleClick() {
+        e.preventDefault()
         console.log(rating)
         submitReview()
     }
-
-
-
-
-return(
-    <form id='Review'>
-        <h1>New Review</h1>
-        <Star_Review  
-            newRating={newRating}
-rating={rating.rating}
-/>
-        <Review_Field 
-            newText={newText}
-/>
-
-        <Button color="danger" type="submit">Submit</Button>
-
-
-    </form>
-)}
-
-
+    return (
+        <form id='Review' onSubmit={handleClick}>
+            <h1>New Review</h1>
+            <Star_Review
+                newRating={newRating}
+                rating={rating.rating}
+            />
+            <Review_Field
+                newText={newText}
+            />
+            <Button color="danger" type="submit" onClick={submitReview}>Submit</Button>
+        </form>
+    )
+}
 export default Review_Form
