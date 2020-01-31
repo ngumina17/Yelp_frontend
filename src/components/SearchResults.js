c9b2427e3fca539d30b2b8870507d6f534d3bcb3
Danny Gleason <danny.c.gleason@gmail.com>
2020-01-31 12:39:49 -0500

    made dollar sign functionality

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -35,7 +35,7 @@ function SearchResults (props){
             <h6>{item.address}</h6>
             <h6>{item.city}, {item.state}</h6>
             <div className="price-wrapper">
-                    <span className="price">Price: (0-5): {item.price}</span>
+                    <span className="price">Price: {item.price==1? <div>$</div>:null}{item.price==2?<div>$$</div>: null}{item.price==3?<div>$$$</div>: null}{item.price==4?<div>$$$$</div>: null}{item.price==5?<div>$$$$$</div>: null}</span>
             </div>  
             </div> 
       </>   
